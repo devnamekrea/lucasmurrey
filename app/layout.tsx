3d59e69edd76f53831757432e22c9bdf6599ea60
te <te@Mac.attlocal.net>
2026-09-24 20:38:00 -0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'Dr. Lucas Murrey - Author, Philosopher, Cultural Critic',
@@ -37,7 +38,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+            <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
